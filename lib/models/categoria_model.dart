e56ae5272715a2b948819b8b638a1205bd1586dd
JuanJose <juanjoseevv@gmail.com>
2024-11-22 18:13:42 -0500

    INICIO DEL PROYECTO

---
+++ lib/models/categoria_model.dart
@@ -0,0 +1,34 @@
+class CategoriaModel {
+  // Atributos de la clase
+  int id;
+  String nombre;
+  String descripcion;
+  String estado;
+
+  // Constructor para inicializar los atributos
+  CategoriaModel(
+      {required this.id,
+      required this.nombre,
+      required this.descripcion,
+      required this.estado});
+
+  // factory es para que se pueda retornar una nueva instancia de la clase
+  factory CategoriaModel.fromJson(Map<String, dynamic> json) {
+    return CategoriaModel(
+      id: json['_id'] ?? '',
+      nombre: json['nombre'] ?? '',
+      descripcion: json['descripcion'] ?? '',
+      estado: json['estado'] ?? '',
+    );
+  }
+
+  // Método para convertir los atributos de la clase a un mapa
+  Map<String, dynamic> toJson() {
+    return {
+      '_id': id,
+      'nombre': nombre,
+      'descripcion': descripcion,
+      'estado': estado,
+    };
+  }
+}
