24834bf629d98403111c2853e792d68edae96477
JuanJose <juanjoseevv@gmail.com>
2024-11-22 18:37:58 -0500

    Agregar controladores para gestionar categorías y productos, incluyendo métodos para listar, crear y actualizar.

---
+++ lib/controllers/categoria_controllers.dart
@@ -0,0 +1,81 @@
+//! Controllers para categorias
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:proyecto_final/models/categoria_model.dart';
+
+class CategoriaControllers {
+  // URL base
+  static const String url = 'https://buildmart-1.onrender.com/api/categorias';
+
+  //! METODOS PARA CATEGORIAS
+
+  // Metodo para obtener todas las categorias (LISTAR)
+  Future<List<CategoriaModel>> getCategorias() async {
+    final response =
+        await http.get(Uri.parse(url)); // Hacer una peticion GET a la API
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonData = jsonDecode(response
+          .body); // Decodificar la respuesta de la API a JSON y guardarla en una variable
+      final List<dynamic> categoriasData = jsonData[
+          'categorias']; // Guardar la lista de categorias en una variable
+
+      return categoriasData
+          .map((item) => CategoriaModel.fromJson(item))
+          .toList(); // Mapear la lista de categorias a una lista de objetos CategoriaModel
+    } else {
+      throw Exception(
+          'Error al obtener las categorias'); // Lanzar un error si no se pudo obtener las categorias
+    }
+  }
+
+  // Metodo para crear una categoria (AGREGAR)
+  Future<void> createCategoria(String nombre, String descripcion,
+      [String estado = 'activo']) async {
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {"Content-Type": "application/json"}, // Cabecera de la peticion
+      body: jsonEncode({
+        'nombre': nombre,
+        'descripcion': descripcion,
+        'estado': estado
+      }), // Cuerpo de la peticion
+    );
+    if (response.statusCode != 200) {
+      throw Exception(
+          'Error al crear la categoria'); // Lanzar un error si no se pudo crear la categoria
+    }
+  }
+
+  // Metodo para actualizar una categoria (ACTUALIZAR)
+  Future<void> updateCategoria(
+      int id, String nombre, String descripcion) async {
+    final response = await http.put(Uri.parse(url),
+        headers: {
+          "Content-Type": "application/json"
+        }, // Cabecera de la peticion
+        body: jsonEncode({
+          'nombre': nombre,
+          'descripcion': descripcion
+        }) // Cuerpo de la peticion
+        );
+    if (response.statusCode != 200) {
+      throw Exception(
+          'Error al actualizar la categoria'); // Lanzar un error si no se pudo actualizar la categoria
+    }
+  }
+
+  // Metodo para actualizar el estado de una categoria (ACTUALIZAR)
+  Future<void> updateEstadoCategoria(int id, String estado) async {
+    final response = await http.put(Uri.parse(url),
+        headers: {
+          "Content-Type": "application/json"
+        }, // Cabecera de la peticion
+        body: jsonEncode({'estado': estado}) // Cuerpo de la peticion
+        );
+    if (response.statusCode != 200) {
+      throw Exception(
+          'Error al actualizar el estado de la categoria'); // Lanzar un error si no se pudo actualizar el estado de la categoria
+    }
+  }
+}
---
+++ lib/controllers/producto_controllers.dart
@@ -0,0 +1,88 @@
+//! Controllers para productos
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:proyecto_final/models/productos_models.dart';
+
+class ProductoControllers {
+  // URL base
+  static const String url = 'https://buildmart-1.onrender.com/api/productos';
+
+  //! METODOS PARA PRODUCTOS
+
+  // Metodo para obtener todos los productos (LISTAR)
+  Future<List<ProductosModels>> getProductos() async {
+    final response =
+        await http.get(Uri.parse(url)); // Hacer una peticion GET a la API
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonData = jsonDecode(response
+          .body); // Decodificar la respuesta de la API a JSON y guardarla en una variable
+      final List<dynamic> productosData = jsonData[
+          'productos']; // Guardar la lista de productos en una variable
+
+      return productosData
+          .map((item) => ProductosModels.fromJson(item))
+          .toList(); // Mapear la lista de productos a una lista de objetos ProductosModels
+    } else {
+      throw Exception(
+          'Error al obtener los productos'); // Lanzar un error si no se pudo obtener los productos
+    }
+  }
+
+  // Metodo para crear un producto (AGREGAR)
+  Future<void> createProducto(String nombre, String categoria,
+      String descripcion, double precio, int stock,
+      [String estado = 'activo']) async {
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {"Content-Type": "application/json"}, // Cabecera de la peticion
+      body: jsonEncode({
+        'nombre': nombre,
+        'categoria': categoria,
+        'descripcion': descripcion,
+        'precio': precio,
+        'stock': stock,
+        'estado': estado
+      }), // Cuerpo de la peticion
+    );
+    if (response.statusCode != 200) {
+      throw Exception(
+          'Error al crear el producto'); // Lanzar un error si no se pudo crear el producto
+    }
+  }
+
+  // Metodo para actualizar un producto (ACTUALIZAR)
+  Future<void> updateProducto(int id, String nombre, String categoria,
+      String descripcion, double precio, int stock) async {
+    final response = await http.put(Uri.parse(url),
+        headers: {
+          "Content-Type": "application/json"
+        }, // Cabecera de la peticion
+        body: jsonEncode({
+          'nombre': nombre,
+          'categoria': categoria,
+          'descripcion': descripcion,
+          'precio': precio,
+          'stock': stock
+        }) // Cuerpo de la peticion
+        );
+    if (response.statusCode != 200) {
+      throw Exception(
+          'Error al actualizar el producto'); // Lanzar un error si no se pudo actualizar el producto
+    }
+  }
+
+  // Metodo para actualizar el estado de un producto (ACTUALIZAR)
+  Future<void> updateEstadoProducto(int id, String estado) async {
+    final response = await http.put(Uri.parse(url),
+        headers: {
+          "Content-Type": "application/json"
+        }, // Cabecera de la peticion
+        body: jsonEncode({'estado': estado}) // Cuerpo de la peticion
+        );
+    if (response.statusCode != 200) {
+      throw Exception(
+          'Error al actualizar el estado del producto'); // Lanzar un error si no se pudo actualizar el estado del producto
+    }
+  }
+}
